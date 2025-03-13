ce47bc6dcfa21dc7769d7808681b025a3d516d3d
kenebebh <kenebebhbanigo@gmail.com>
2025-03-13 15:52:56 +0100

    installed tailwind and shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <p className="text-blue-700 font-extrabold text-6xl">Hello world</p>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
