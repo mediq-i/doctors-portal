0c6870f558f21d688bb81372eaa4380cedb2d655
kenebebh <kenebebhbanigo@gmail.com>
2025-04-01 02:48:36 +0100

    restored the onboarding form to the personal-professional-info page

--- src/routes/onboarding/personal-professional-information.tsx
+++ src/routes/onboarding/personal-professional-information.tsx
@@ -1,7 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Onboarding from "@/components/partials/onboarding-wrapper";
-import { UploadUniversityDegree } from "@/components/onboarding";
-import { useOnboardingProgressStore } from "@/store/onboarding-progress";
 
 export const Route = createFileRoute(
   "/onboarding/personal-professional-information"
@@ -10,23 +8,5 @@ export const Route = createFileRoute(
 });
 
 function RouteComponent() {
-  const { goToNextStep, formData, updateFormData } =
-    useOnboardingProgressStore();
-
-  // Handle form submission for each step
-  const handleStepSubmit = (data?: any) => {
-    if (data) {
-      updateFormData(data);
-    }
-    goToNextStep(); //update progress bar
-    goToNextStep(); //moves to the next step in the personal-professional-info form
-  };
-
-  // return <Onboarding />;
-  return (
-    <UploadUniversityDegree
-      onSubmit={handleStepSubmit}
-      defaultValues={formData}
-    />
-  );
+  return <Onboarding />;
 }
