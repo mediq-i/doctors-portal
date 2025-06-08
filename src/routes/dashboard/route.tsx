2ffc02c13b84cd517fd36a2f83ea1138e943de25
Alvin Okafor <alvin2k99@gmail.com>
2025-06-08 22:27:49 +0100

    fix: fixed verification, fixed bank details not updating

--- src/routes/dashboard/route.tsx
+++ src/routes/dashboard/route.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { DashboardLayout } from "@/layouts";
 import {
-  WalletSection,
+  // WalletSection,
   AvailabilitySection,
   AppointmentsSection,
 } from "@/components/dashboard";
@@ -26,7 +26,6 @@ function RouteComponent() {
       ServiceProviderAdapter.getServiceProviderDetails({ id: userId! }),
     enabled: !!userId,
   });
-
   const providerName = providerData
     ? `${providerData.data.first_name} ${providerData.data.last_name}`
     : "--";
@@ -39,8 +38,22 @@ function RouteComponent() {
         <div className="space-y-8 max-w-5xl mx-auto w-full">
           <div>
             <h1 className="text-2xl md:text-3xl font-bold">Dashboard</h1>
-            <p className="text-muted-foreground text-base md:text-lg">
-              Hello, Dr. {providerName}
+            <p className="text-muted-foreground text-base md:text-lg flex items-center gap-2">
+              Dr. {providerName}
+              {isVerified === true && (
+                <svg
+                  width="16"
+                  height="17"
+                  viewBox="0 0 12 13"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M11.5 6.5C11.5 5.6978 11.0063 5.00219 10.2715 4.62634C10.5295 3.84659 10.3837 2.99951 9.81707 2.43293C9.25049 1.86634 8.40341 1.72049 7.62366 1.97854C7.25341 1.24366 6.5522 0.75 5.75 0.75C4.9478 0.75 4.2522 1.24366 3.88195 1.97854C3.09659 1.72049 2.24951 1.86634 1.68293 2.43293C1.11634 2.99951 0.976098 3.84659 1.23415 4.62634C0.499268 5.00219 0 5.6978 0 6.5C0 7.3022 0.499268 7.99781 1.23415 8.37366C0.976098 9.15341 1.11634 10.0005 1.68293 10.5671C2.24951 11.1337 3.09659 11.2739 3.87634 11.0215C4.25219 11.7563 4.9478 12.25 5.75 12.25C6.5522 12.25 7.25341 11.7563 7.62366 11.0215C8.40341 11.2739 9.25049 11.1337 9.81707 10.5671C10.3837 10.0005 10.5295 9.15341 10.2715 8.37366C11.0063 7.99781 11.5 7.3022 11.5 6.5ZM4.93098 8.8561L2.83293 6.75805L3.6239 5.96146L4.89171 7.22927L7.58439 4.29537L8.40902 5.05829L4.93098 8.8561Z"
+                    fill="#3697F1"
+                  />
+                </svg>
+              )}
             </p>
           </div>
 
@@ -59,7 +72,7 @@ function RouteComponent() {
             </Alert>
           )}
 
-          <WalletSection />
+          {/* <WalletSection /> */}
           <AvailabilitySection />
           <AppointmentsSection />
         </div>
