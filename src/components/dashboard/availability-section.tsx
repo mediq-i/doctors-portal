c85af3509447dfbc7822f73bab5f42613389db20
Alvin Okafor <alvin2k99@gmail.com>
2025-07-07 12:26:06 +0100

    changed appointment time to 30min increments

--- src/components/dashboard/availability-section.tsx
+++ src/components/dashboard/availability-section.tsx
@@ -29,14 +29,26 @@ const DAYS = [
 ] as const;
 
 const TIME_SLOTS = [
-  { display: "8:00 AM - 9:00 AM", start: "08:00", end: "09:00" },
-  { display: "9:00 AM - 10:00 AM", start: "09:00", end: "10:00" },
-  { display: "10:00 AM - 11:00 AM", start: "10:00", end: "11:00" },
-  { display: "11:00 AM - 12:00 PM", start: "11:00", end: "12:00" },
-  { display: "1:00 PM - 2:00 PM", start: "13:00", end: "14:00" },
-  { display: "2:00 PM - 3:00 PM", start: "14:00", end: "15:00" },
-  { display: "3:00 PM - 4:00 PM", start: "15:00", end: "16:00" },
-  { display: "4:00 PM - 5:00 PM", start: "16:00", end: "17:00" },
+  { display: "8:00 AM - 8:30 AM", start: "08:00", end: "08:30" },
+  { display: "8:30 AM - 9:00 AM", start: "08:30", end: "09:00" },
+  { display: "9:00 AM - 9:30 AM", start: "09:00", end: "09:30" },
+  { display: "9:30 AM - 10:00 AM", start: "09:30", end: "10:00" },
+  { display: "10:00 AM - 10:30 AM", start: "10:00", end: "10:30" },
+  { display: "10:30 AM - 11:00 AM", start: "10:30", end: "11:00" },
+  { display: "11:00 AM - 11:30 AM", start: "11:00", end: "11:30" },
+  { display: "11:30 AM - 12:00 PM", start: "11:30", end: "12:00" },
+  { display: "12:00 PM - 12:30 PM", start: "12:00", end: "12:30" },
+  { display: "12:30 PM - 1:00 PM", start: "12:30", end: "13:00" },
+  { display: "1:00 PM - 1:30 PM", start: "13:00", end: "13:30" },
+  { display: "1:30 PM - 2:00 PM", start: "13:30", end: "14:00" },
+  { display: "2:00 PM - 2:30 PM", start: "14:00", end: "14:30" },
+  { display: "2:30 PM - 3:00 PM", start: "14:30", end: "15:00" },
+  { display: "3:00 PM - 3:30 PM", start: "15:00", end: "15:30" },
+  { display: "3:30 PM - 4:00 PM", start: "15:30", end: "16:00" },
+  { display: "4:00 PM - 4:30 PM", start: "16:00", end: "16:30" },
+  { display: "4:30 PM - 5:00 PM", start: "16:30", end: "17:00" },
+  { display: "5:00 PM - 5:30 PM", start: "17:00", end: "17:30" },
+  { display: "5:30 PM - 6:00 PM", start: "17:30", end: "18:00" },
 ];
 
 interface CalendarDay {
